f0265dae4521fd8b15101b9eef735be363a2bc00
JaimeYi <jguzman@usm.cl>
2024-06-01 19:21:47 -0400

    todas las funciones y los metodos listos(hay que revisar la altura que se devuelve en el metodo 'recortar')

--- main_code.cpp
+++ main_code.cpp
@@ -22,15 +22,65 @@ void insertar(int i, string str, super_string& s){
     }
 }
 
+void reverse(super_string& s, int l, int r){
+    super_string a, b, c, d;
+    string aux = s.stringizar();
+    int largo = aux.length();
+    if (l == 0 && r == largo-1){
+        s.reverso();
+    } else if (r == largo-1){
+        s.separar(l, a, b);
+        b.reverso();
+        a.juntar(b);
+        s = a;
+    } else {
+        s.separar(l, a, b);
+        b.separar((r-l), c, d);
+        c.reverso();
+        a.juntar(c);
+        a.juntar(d);
+        s = a;
+    }
+}
+
+void eliminar(super_string& s, int l, int r){
+    super_string a, b, c ,d;
+    string aux = s.stringizar();
+    int largo = aux.length();
+    if (l == 0 && r == largo-1){
+        s.limpiar();
+    } else if (r == largo-1){
+        s.separar(l, a, b);
+        s = a;
+    } else {
+        s.separar(l, a, b);
+        b.separar((r-l), c, d);
+        a.juntar(d);
+        s = a;
+    }
+}
+
+void recorte(super_string& s){
+    int i = s.recortar();
+    cout << i << endl;
+}
 
 int main (){
     super_string xd, m, ya, pr, s;
     string hola, prueba , ja;
 
-    insertar(0, "Gato_", xd);
+    insertar(0, "El_Carlos", xd);
     mostrar(xd);
-    insertar(5, "amarillo_", xd);
+    insertar(9, "_es_malo", xd);
     mostrar(xd);
+    eliminar(xd, 13, 16);
+    mostrar(xd);
+    insertar(13, "bueno", xd);
+    mostrar(xd);
+    recorte(xd);
+    reverse(xd, 0, 17);
+    mostrar(xd);
+    recorte(xd);
 
     return 0;
 }
