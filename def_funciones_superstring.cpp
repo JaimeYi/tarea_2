bc22e1b8f3ce2917a215e3101edddfe93d20245d
JaimeYi <jguzman@usm.cl>
2024-06-02 01:21:26 -0400

    todo comentado(tarea 100% lista)

--- def_funciones_superstring.cpp
+++ def_funciones_superstring.cpp
@@ -1,5 +1,17 @@
 #include "tda_superstring.cpp"
-#include <math.h>
+
+
+/*****
+* void juntar
+******
+* Esta función une un ABB dado, al ABB desde el que se solicita la función la función
+******
+* Input:
+* super_string &s : ABB que sera unido al ABB desde el que se callea la función
+******
+* Returns:
+* void, no retorna nada
+*****/
 
 void super_string::juntar(super_string &s){
     if (s.root == nullptr){
@@ -8,6 +20,64 @@ void super_string::juntar(super_string &s){
     s.aumentarIndices(s.root, length);
     joinTrees(s.root);
 }
+
+/*****
+* void aumentarIndices
+******
+* esta función se utiliza en el metodo 'juntar', aumenta el indice de cada nodo del ABB a juntar, para
+* que asi se agregue de forma correcta el segundo ABB al primero
+******
+* Input:
+* nodo* nodo : nodo al que se le aumentara el indice
+* int largo : largo del primer ABB para que los indices del segundo ABB comiencen justo al final de los 
+*             del primer ABB
+* Returns:
+* void, no retorna nada
+*****/
+
+void super_string::aumentarIndices(nodo* nodo, int largo){
+    if (nodo == nullptr){
+        return;
+    }
+    aumentarIndices(nodo->left, largo);
+    nodo->index += largo;
+    aumentarIndices(nodo->right, largo);
+}
+
+/*****
+* void joinTrees
+******
+* se recorre el ABB(2) que se quiere unir al ABB(1) original en in-order y se 
+* agrega cada caracter del ABB(2) al ABB(1)
+******
+* Input:
+* nodo* nodo : nodo del que se sacara el caracter para agregarlo al ABB(1)
+******
+* Returns:
+* void, no retorna nada
+*****/
+
+void super_string::joinTrees(nodo* nodo){
+    if (nodo == nullptr){
+        return;
+    }
+    joinTrees(nodo->left);
+    this->agregar(nodo->c);
+    joinTrees(nodo->right);
+}
+
+/*****
+* void agregar
+******
+* esta función agregara siempre los nodos al final del ABB, por esa 
+* condicion es que en la funcion solo se recorre el ABB a traves de los hijos a la derecha de cada nodo
+******
+* Input:
+* char c : caracter que se agregara al ABB en forma de nodo
+******
+* Returns:
+* void, no retorna nada
+*****/
 
 void super_string::agregar(char c){
     if (root == nullptr){
@@ -24,6 +94,22 @@ void super_string::agregar(char c){
     }
     length++;
 }
+
+/*****
+* void separar
+******
+* esta función pide dos super_strings y un indice donde el superstring se partira en 2, en el primer super_string se guarda
+* desde la mitad del super_string desde el que se solicita la función hasta el inicio de este mismo, y en el segundo super_string
+* se guarda desde la mitad del super_string desde el que se solicita la función hasta el final del mismo.
+******
+* Input:
+* int i : representa la posición donde se realizara la división del ABB
+* super_string& a : en este parametro se almacenara la parte izquierda de la división
+* super_string& b : en este parametro se almacenara la parte derecha de la división
+******
+* Returns:
+* void, no retorna nada
+*****/
 
 void super_string::separar(int i, super_string &a, super_string &b){
     if (i <= 0){
@@ -49,9 +135,79 @@ void super_string::separar(int i, super_string &a, super_string &b){
     }
 }
 
+/*****
+* void appendArrayInOrder
+******
+* en esta función se recorre el ABB in-order y cada nodo del ABB se agrega a un array dado en la función recortar
+******
+* Input:
+* nodo* node: nodo que se guardara en el array
+* nodo** arr: array donde se agregan los nodos encontrados
+* int& i: contador partido en 0 que servira para ir avanzando posiciones en el array al momento de asignar los nodos en este
+* Returns:
+* void, no retorna nada
+*****/
+
+void super_string::appendArrayInOrder(nodo* node, nodo** arr, int& i){
+    if (node == nullptr){
+        return;
+    }
+
+    appendArrayInOrder(node->left, arr, i);
+    arr[i++] = node;
+    appendArrayInOrder(node->right, arr, i);
+}
+
+/*****
+* void reverso
+******
+* la función reverso invierte el orden de todos los nodos del ABB desde el que se solicitó
+******
+* Input:
+* no recibe parametros de entrada
+******
+* Returns:
+* void, no retorna nada
+*****/
+
 void super_string::reverso(){
     reverseNodes(root);
 }
+
+/*****
+* void reverseNodes
+******
+* función base de la función 'reversa', en cada iteracion de la recursividad de la función se intercambia el nodo
+* derecho de un nodo dado con el nodo izquierdo del mismo
+******
+* Input:
+* nodo* nodo : nodo donde se realizara las operaciones de la función
+******
+* Returns:
+* void, no retorna nada
+*****/
+
+void super_string::reverseNodes(nodo* nodo){
+    if (nodo == nullptr){
+        return;
+    }
+    swap(nodo->left, nodo->right);
+    reverseNodes(nodo->left);
+    reverseNodes(nodo->right);
+}
+
+/*****
+* int recortar
+******
+* la función recortar realiza un balanceo del super_string desde el ABB que se solicita para que asi el costo operacional al recorrer 
+* el camino más largo pase de O(n) a O(log n)
+******
+* Input:
+* no recibe parametros de entrada
+******
+* Returns:
+* int, el retorno entrega la altura del arbol despues de haberse realizado la operacion de recorte
+*****/
 
 int super_string::recortar(){
     nodo** arr = new nodo*[length];
@@ -67,15 +223,112 @@ int super_string::recortar(){
     return height;
 }
 
+/*****
+* nodo* balancearABBbasedInArray
+******
+* la función busca el nodo que se encuentre a la mitad, habiendo ordenando los nodos de menor a mayor anteriormente, estableciendo
+* el nodo central como padre de los siguientes 2 nodos que se encuentren en la mitad de cada 'sub array' que queda si el array
+* lo separamos en la mitad y asi recursivamente hasta terminar de armar denuevo el ABB
+******
+* Input:
+* nodo** arr : array donde se ira buscando el valor medio
+* int start : valor donde inicia el array (en el sub array derecho se usara el valor medio obtenido + 1 como valor donde inicia el array)
+* int end: valor donde inicia el array (en el sub array izquierdo se usara el valor medio obtenido -1 como valor donde termina el array)
+* Returns:
+* nodo*, se retorna el nodo que sera la nueva raiz del ABB
+*****/
+
+super_string::nodo* super_string::balancearABBbasedInArray(nodo** arr, int start, int end){
+    if (start > end){
+        return nullptr;
+    }
+    int mid = start + (end - start)/2;
+    nodo* nodo = arr[mid];
+    nodo->left = balancearABBbasedInArray(arr, start, mid-1);
+    nodo->right = balancearABBbasedInArray(arr, mid+1, end);
+
+    return nodo;
+}
+
+/*****
+* void calcularAlturaLeft
+******
+* por la forma en la que se creara el ABB el nodo con mayor altura siempre estara lo más a la izquierda o
+* derecha posible dentro del arbol, por lo que en este caso se recorre cada nodo izquierdo posible, y por 
+* cada iteracion de la función se sumara 1 al contador
+******
+* Input:
+* nodo* nodo : nodo donde se buscara el nodo izquierdo
+* int& v : contador donde se guardara la altura final obtenida en la función
+******
+* Returns:
+* void, no retorna nada
+*****/
+
+void super_string::calcularAlturaLeft(nodo* nodo, int& v){
+    if (nodo == nullptr){
+        return;
+    }
+
+    v+=1;
+    calcularAlturaLeft(nodo->left, v);
+}
+
+/*****
+* void calcularAlturaRight
+******
+* por la forma en la que se creara el ABB el nodo con mayor altura siempre estara lo más a la izquierda o
+* derecha posible dentro del arbol, por lo que en este caso se recorre cada nodo derecho posible, y por
+* cada iteracion de la función se sumara 1 al contador
+******
+* Input:
+* nodo* nodo : nodo donde se buscara el nodo derecho
+* int& v : contador donde se guardara la altura final obtenida en la función
+******
+* Returns:
+* void, no retorna nada
+*****/
+
+void super_string::calcularAlturaRight(nodo* nodo, int& v){
+    if (nodo == nullptr){
+        return;
+    }
+
+    v+=1;
+    calcularAlturaRight(nodo->right, v);
+}
+
+/*****
+* string stringizar
+******
+* esta función recorre el ABB y va agregando el caracter de cada nodo a un string vacio para obtener el string contenido en el ABB
+******
+* Input:
+* no recibe parametros de entrada
+******
+* Returns:
+* string, sera el string que estaba contenido en el ABB
+*****/
+
 string super_string::stringizar(){
     string str = "";
     stringizar_in_order(root, str);
     return str;
 }
 
-void super_string::limpiar(){
-    limpiarPostOrder(root);
-}
+/*****
+* void stringizar_in_order
+******
+* esta funcion es la base de la función 'stringizar', se recorre el ABB in-order de forma recursiva y
+* se va sumando cada caracter encontrado al string vacio dado en la función stringizar
+******
+* Input:
+* nodo* nodo : el caracter incluido en este nodo sera agregado al string dado 
+* string& str: es el string dado en la función 'stringizar'
+******
+* Returns:
+* void, no retorna nada
+*****/
 
 void super_string::stringizar_in_order(nodo* nodo, string& str){
     if (nodo == nullptr){
@@ -85,6 +338,35 @@ void super_string::stringizar_in_order(nodo* nodo, string& str){
     str += nodo->c;
     stringizar_in_order(nodo->right, str);
 }
+
+/*****
+* void limpiar
+******
+* esta función deja al ABB sin nodos
+******
+* Input:
+* no recibe parametros de entrada
+******
+* Returns:
+* void, no retorna nada
+*****/
+
+void super_string::limpiar(){
+    limpiarPostOrder(root);
+}
+
+/*****
+* void limpiarPostOrder
+******
+* la función 'limpiar' se basa en esta función, en esta función se recorre el ABB en post-order para asi evitar 
+* eliminar la raiz del ABB antes de eliminar el resto de nodos, se elimina cada nodo al que se llega
+******
+* Input:
+* nodo* nodo : nodo que sera eliminado
+******
+* Returns:
+* void, no retorna nada
+*****/
 
 void super_string::limpiarPostOrder(nodo* nodo){
     if (nodo == nullptr){
@@ -99,71 +381,3 @@ void super_string::limpiarPostOrder(nodo* nodo){
         delete nodo;
     }
 }
-
-void super_string::appendArrayInOrder(nodo* node, nodo** arr, int& i){
-    if (node == nullptr){
-        return;
-    }
-
-    appendArrayInOrder(node->left, arr, i);
-    arr[i++] = node;
-    appendArrayInOrder(node->right, arr, i);
-}
-
-super_string::nodo* super_string::balancearABBbasedInArray(nodo** arr, int start, int end){
-    if (start > end){
-        return nullptr;
-    }
-    int mid = start + (end - start)/2;
-    nodo* nodo = arr[mid];
-    nodo->left = balancearABBbasedInArray(arr, start, mid-1);
-    nodo->right = balancearABBbasedInArray(arr, mid+1, end);
-
-    return nodo;
-}
-
-void super_string::joinTrees(nodo* nodo){
-    if (nodo == nullptr){
-        return;
-    }
-    joinTrees(nodo->left);
-    this->agregar(nodo->c);
-    joinTrees(nodo->right);
-}
-
-void super_string::reverseNodes(nodo* nodo){
-    if (nodo == nullptr){
-        return;
-    }
-    swap(nodo->left, nodo->right);
-    reverseNodes(nodo->left);
-    reverseNodes(nodo->right);
-}
-
-void super_string::aumentarIndices(nodo* nodo, int largo){
-    if (nodo == nullptr){
-        return;
-    }
-    aumentarIndices(nodo->left, largo);
-    nodo->index += largo;
-    aumentarIndices(nodo->right, largo);
-}
-
-void super_string::calcularAlturaLeft(nodo* nodo, int& v){
-    if (nodo == nullptr){
-        return;
-    }
-
-    v+=1;
-    calcularAlturaLeft(nodo->left, v);
-}
-
-void super_string::calcularAlturaRight(nodo* nodo, int& v){
-    if (nodo == nullptr){
-        return;
-    }
-
-    v+=1;
-    calcularAlturaRight(nodo->right, v);
-}
-/* cd /mnt/c/Users/Under/Codes/C++/Tareas */
--- main_code.cpp
+++ main_code.cpp
@@ -1,9 +1,36 @@
 #include <fstream>
 #include "def_funciones_superstring.cpp"
 
+/*****
+* void mostrar
+******
+* convierte el super string dado en string usando el metodo 'stringizar' e imprime en pantalla este string
+******
+* Input:
+* super_string& s : super_string a mostrar
+* Returns:
+* void, no retorna nada
+*****/
+
 void mostrar(super_string& s){
     cout << s.stringizar() << endl;
 }
+
+/*****
+* void insertar
+******
+* esta función convierte un string dado como parametro en super string, luego separa el super string dado
+* en la posición indicada, de esta forma se van a tener 3 super strings (a, b, nuevo), ya teniendo estos
+* para mantener el orden se juntaran en el orden: a->nuevo, a->b. Y en caso de tener que hacer la 
+* inserción al inicio del super string se juntara solo nuevo->b. 
+******
+* Input:
+* int i : posición donde se realizara la división del super string
+* string str : string que se convertira en super string y se juntara al super string s
+* super_string& s : super string al que se unira el nuevo string
+* Returns:
+* void, no retorna nada
+*****/
 
 void insertar(int i, string str, super_string& s){
     super_string a, b, nuevo;
@@ -22,6 +49,22 @@ void insertar(int i, string str, super_string& s){
     }
 }
 
+/*****
+* void reverse
+******
+* esta funcion crea 3 super strings (menos cuando se reversa todo el super string, cuando
+* se reversa de cierta posición hasta el final o cuando se reversa desde el inicio hasta 
+* el final) donde reversa el super string central y despues vuelve a unirlos todos
+******
+* Input:
+* super_string& s : super string donde se trabajara en la función
+* int l : indice desde donde se reversara el super string
+* int r : indice hasta donde se reversara el super string
+******
+* Returns:
+* void, no retorna nada
+*****/
+
 void reverse(super_string& s, int l, int r){
     super_string a, b, c, d;
     string aux = s.stringizar();
@@ -31,6 +74,11 @@ void reverse(super_string& s, int l, int r){
     } else if (r == largo-1){
         s.separar(l, a, b);
         b.reverso();
+        a.juntar(b);
+        s = a;
+    } else if(l == 0){
+        s.separar(r, a, b);
+        a.reverso();
         a.juntar(b);
         s = a;
     } else {
@@ -43,6 +91,22 @@ void reverse(super_string& s, int l, int r){
     }
 }
 
+/*****
+* void eliminar
+******
+* esta funcion crea 3 super strings (menos cuando se elimina todo el super string, cuando
+* se elimina de cierta posición hasta el final o cuando se elimina desde el inicio hasta 
+* el final) donde se elimina el super string central y se juntan los dos restantes
+******
+* Input:
+* super_string& s : super string donde se trabajara en la función
+* int l : indice desde donde se eliminara el super string
+* int r : indice hasta donde se eliminara el super string
+******
+* Returns:
+* void, no retorna nada
+*****/
+
 void eliminar(super_string& s, int l, int r){
     super_string a, b, c ,d;
     string aux = s.stringizar();
@@ -52,6 +116,9 @@ void eliminar(super_string& s, int l, int r){
     } else if (r == largo-1){
         s.separar(l, a, b);
         s = a;
+    } else if (l == 0){
+        s.separar(r, a, b);
+        s = b;
     } else {
         s.separar(l, a, b);
         b.separar((r-l), c, d);
@@ -59,6 +126,20 @@ void eliminar(super_string& s, int l, int r){
         s = a;
     }
 }
+
+/*****
+* void recorte
+******
+* esta función reduce el costo operacional del camino más largo de O(n) a 
+* O(log n) mediante el uso del metodo recortar, luego de realizar esta 
+* operación se imprime el nuevo alto del arbol en pantalla
+******
+* Input:
+* super_string& s : super string a recortar
+******
+* Returns:
+* void, no retorna nada
+*****/
 
 void recorte(super_string& s){
     int i = s.recortar();
@@ -95,6 +176,8 @@ int main (){
             reverse(s, i, j);
         }
     }
+
+    file.close();
 
     return 0;
 }
